6032b083b0dc492b40c0c54872e1259ec6bb94fa
Wojciech Pospieszyski <pospieszynski.wojciech@gmail.com>
2016-07-14 21:39:10 +0200

    add prod to warehouse sppec added

--- lib/services/price_gross.rb
+++ lib/services/price_gross.rb
@@ -7,3 +7,4 @@ module Shop
     end
   end
 end
+vk
---
+++ spec/requests/warehouse/delete_commodity_from_warehouse_spec.rb
@@ -0,0 +1,50 @@
+require "web_helper"
+
+RSpec.describe "POST /basket_remove", type: :request do
+  context "invalid params" do
+    let(:invalid_params) { {foo: 'bar'} }
+
+    before do
+      do_request(invalid_params)
+    end
+
+    it "returns 422 HTTP status code" do
+      expect(last_response.status).to eql(422)
+    end
+  end
+
+  context "valid params" do
+    let(:params) { {"product_id" => 1, "quantity" => 1} }
+    let(:domain) { "http://example.org" }
+
+    before do
+      Shop::AddProductToBasket.new.call(1, 2)
+      do_request(params)
+    end
+
+    it "returns 200 HTTP status code" do
+      follow_redirect!
+      expect(last_response.status).to eql(200)
+    end
+
+    it "redirects to BASKET" do
+      follow_redirect!
+      expect(last_request.url). to eql(domain + "/basket")
+    end
+
+    it "calls Remove Product From Basket with proper params" do
+      Shop::AddProductToBasket.new.call(1, 2)
+      remover = Shop::RemoveProductFromBasket.new
+      expect(Shop::RemoveProductFromBasket).to receive(:new).and_return remover
+      expect(remover).to receive(:call).with(1).and_call_original
+      do_request(params)
+    end
+  end
+
+
+  private
+
+  def do_request(params={})
+    post '/basket_remove', params
+  end
+end
